4f6c4aa54a292cbedf99946be2009dc0aead75a7
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-07 20:06:35 +0530

    [Siddharth]Add: Total Employee Wage For Multiple Company

---
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+
+namespace EmployeeWageProblem
+{
+
+    public class EmployeeWage
+    {
+        const int FULL_DAY_HR = 8, HALF_DAY_HR = 4, IS_PRESENT = 0, IS_FULL_TIME = 1, IS_PART_TIME = 2, Company_Name=0;
+        public static int EmpWage(string Company_Name, int WAGE_PER_HR, int TOTAL_WORKING_DAYS, int TOTAL_WORKING_HRS)
+       
+        {
+                int totalEmpWage = 0, empHrs = 0;
+                for (int i = 0; i < TOTAL_WORKING_DAYS && empHrs < TOTAL_WORKING_HRS; i++)
+                {
+                    Random random = new Random();
+                    int empCheck = random.Next(0, 3);
+                    switch (empCheck)
+                    {
+                        case IS_FULL_TIME:
+                            empHrs += FULL_DAY_HR;
+                            break;
+                        case IS_PART_TIME:
+                            empHrs += HALF_DAY_HR;
+                            break;
+                        default:
+                            empHrs += 0;
+                            break;
+                    }
+                }
+            totalEmpWage = WAGE_PER_HR * empHrs;
+            Console.WriteLine("Total Employee Wage For Company " + Company_Name + " is : " + totalEmpWage);
+            return totalEmpWage;
+            }
+        }
+    }
--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -4,38 +4,11 @@ namespace EmployeeWageProblem
 {
     class Program
     {
-       
-         public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage problem statement ");
-            Console.WriteLine("1. Employee Attendace" + "\n" + "2. Daily Employee Wage" + "\n" + "3. Part Time Employee Wage" + "\n" + "4. Monthly Wage" + "\n" + "5. Condition Working Hours");
-            Console.WriteLine("Enter The Above Mention Option");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-           
-            {
-                case 1:
-                    EmployeeAttendance attendance = new EmployeeAttendance();
-                    attendance.Attendance();
-                    break;
-                case 2:
-                    DailyEmployeeWage employee = new DailyEmployeeWage();
-                    employee.EmpWage();
-                    break;
-                case 3:
-                    PartTimeEmployeeWage partTime = new PartTimeEmployeeWage();
-                    partTime.PartTime();
-                    break;
-                case 4:
-                    MonthlyWage working = new MonthlyWage();
-                    working.WorkingDay();
-                    break;
-                case 5:
-                    ConditionWorkingHours condition = new ConditionWorkingHours();
-                    condition.Condition();
-                    break;
-            }
-
+            Console.WriteLine("Welcome to Employee Wage Problem statement");
+            EmployeeWage.EmpWage("Byjus", 20, 22, 25);
+            EmployeeWage.EmpWage("Scaler", 20, 22, 25);
         }
     }
 }
