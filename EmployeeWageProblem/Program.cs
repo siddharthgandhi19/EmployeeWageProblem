875dde13feb518ef2c1c09a732334db21cc3bbdc
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-07 18:26:52 +0530

    [Siddharth]Add: Employee Attendance

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmployeeWagesProblem;
+using System;
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage problem statement ");
+            EmployeeWage employee = new EmployeeWage();
+            employee.Attendance();
+        }
+    }
+}
